65833c17048cf6a179e0dd87fba9e512f21717b7
Lauren Lancaster <737lanc@gmail.com>
2023-09-11 16:04:35 +1200

    setup firebase index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD50OUt469cfIPHpViDXV_5xVgd5lbFmlI",
-  authDomain: "welly-horns.firebaseapp.com",
-  projectId: "welly-horns",
-  storageBucket: "welly-horns.appspot.com",
-  messagingSenderId: "1097037308100",
-  appId: "1:1097037308100:web:f00451ecce9d2e8736e991",
-  measurementId: "G-DYHH3V74ZS"
-};
+  apiKey: 'AIzaSyD50OUt469cfIPHpViDXV_5xVgd5lbFmlI',
+  authDomain: 'welly-horns.firebaseapp.com',
+  projectId: 'welly-horns',
+  storageBucket: 'welly-horns.appspot.com',
+  messagingSenderId: '1097037308100',
+  appId: '1:1097037308100:web:f00451ecce9d2e8736e991',
+  measurementId: 'G-DYHH3V74ZS',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 async function getCities(db) {
   // const citiesCol = collection(db, 'cities');
