1f9adb6ffe6137e3cf77289a8569a51931c8012c
Lauren Lancaster <737lanc@gmail.com>
2023-09-25 15:57:27 +1300

    add react-scroll

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,7 +1,22 @@
 import { Link } from 'react-router-dom'
+import { Link as LinkScroll } from 'react-scroll'
 
 function Nav() {
-  return <></>
+  return (
+    <>
+      <div id="btn-container">
+        <Link to="/">
+          <button className="nav-btn">Home</button>
+        </Link>
+        <Link to="/profile">
+          <button className="nav-btn">Profile</button>
+        </Link>
+        <LinkScroll to="/events">
+          <button className="nav-btn">Profile</button>
+        </LinkScroll>
+      </div>
+    </>
+  )
 }
 
 export default Nav
